9c58a086c5771364747d66e273e58e7fd2877aec
RKM170 <kumarmishrarishi8@gmail.com>
2026-04-13 20:15:39 +0530

    saving boilerplate code and text

---
+++ advanced css properties/index.js
@@ -0,0 +1,7 @@
+console.log("rishi");
+// alert("hello duniya");
+
+// let a = prompt("enter no1","");
+// let b = prompt("enter no2","");
+let sum = parseInt(a) + parseInt(b);
+console.log(`the sum of ${a} and ${b} = ${sum}`);
